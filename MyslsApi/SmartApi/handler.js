21049a6e733d287c789c3f8dd42bf3b090b4f754
prashant.gaikwad <prashant.gaikwad@accenture.com>
2016-08-02 13:55:47 -0500

    Changed Handler code

--- MyslsApi/SmartApi/handler.js
+++ MyslsApi/SmartApi/handler.js
@@ -2,6 +2,6 @@
 
 module.exports.handler = function(event, context, cb) {
   return cb(null, {
-    message: 'Go Serverless! Your Lambda function executed successfully!'
+    message: 'Go Serverless! Your Smart Api function executed successfully!'
   });
 };
